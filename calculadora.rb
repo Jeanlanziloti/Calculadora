05d22aaf37112da2957cd7bf91d195d82e154508
Jean Lanziloti <jean.lanziloti@gmail.com>
2012-07-21 15:42:08 -0300

    Adicionado método para retornar se o número é primo ou não

--- calculadora.rb
+++ calculadora.rb
@@ -1,7 +1,25 @@
+# coding: utf-8
+
 class Calculadora
 
 	def	soma(n1, n2)
 		n1 + n2
+	end
+
+	def primos(numero)
+		contador = 0;
+		
+		(1..numero).each do |n|
+			if numero % 2 == 0
+				contador += 1
+			end
+		end
+
+		if contador <= 2
+			"#{numero} é número primo"
+		else
+			"#{numero} não é número primo"
+		end
 	end
 
 end
